bdc7e8af052bccde387ee698a488198b920374a9
angelsl <angelsl@in04.sg>
2020-09-29 03:38:36 +0800

    Specify CMake toolchain file

--- .docker_build.sh
+++ .docker_build.sh
@@ -11,5 +11,6 @@ cmake "$SCRIPT_DIR/sling/linux" \
   -DCMAKE_BUILD_TYPE=Release \
   -DSLING_SINTERHOST_CUSTOM_SOURCES=../deps/sinter/devices/ev3/src/ev3_functions.c \
   -DSLING_SINTERHOST_CUSTOM=../../../host_custom.c \
-  -DSLING_SINTERHOST_PRERUN=../../../host_prerun.c
+  -DSLING_SINTERHOST_PRERUN=../../../host_prerun.c \
+  -DCMAKE_TOOLCHAIN_FILE=/home/compiler/toolchain-armel.cmake
 make -j$(nproc)
