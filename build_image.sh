e04306a9e5b219f5be891995d3baf3eb9b908f54
angelsl <angelsl@in04.sg>
2020-09-29 05:03:31 +0800

    Increase image size

--- build_image.sh
+++ build_image.sh
@@ -11,7 +11,7 @@ cp ../build-ev3/{sling,sinter_host} .
 
 docker build -t sourceacademy/ev3-source .
 
-TMPDIR=. # $(mktemp -d)
+TMPDIR=$(mktemp -d)
 TARFILE="$TMPDIR/ev3-source.tar"
 IMGFILE="$TMPDIR/ev3-source.img"
 
@@ -19,11 +19,11 @@ bash "$SCRIPT_DIR/brickstrap.sh" create-tar sourceacademy/ev3-source "$TARFILE"
 
 docker rmi sourceacademy/ev3-source
 
-export BRICKSTRAP_IMAGE_FILE_SIZE=$(echo $(($(du -m "$TARFILE" | cut -f1)*5/4)) | cut -d. -f1)M
+export BRICKSTRAP_IMAGE_FILE_SIZE=$(echo $(($(du -m "$TARFILE" | cut -f1)*2)) | cut -d. -f1)M
 echo 'Using BRICKSTRAP_IMAGE_FILE_SIZE='$BRICKSTRAP_IMAGE_FILE_SIZE
 
 bash "$SCRIPT_DIR/brickstrap.sh" create-image "$TARFILE" "$IMGFILE"
 
 mv "$IMGFILE" "$SCRIPT_DIR"
-# rm -rf "$TMPDIR"
+rm -rf "$TMPDIR"
 >&2 echo "Image created at $SCRIPT_DIR/$(basename "$IMGFILE")"
