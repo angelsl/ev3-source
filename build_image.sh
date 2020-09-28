b3b0d4601824b1d3284d596252ea5b1ab0e7af79
angelsl <angelsl@in04.sg>
2020-09-29 03:35:22 +0800

    Don't send Docker context and don't allocate TTY

--- build_image.sh
+++ build_image.sh
@@ -5,5 +5,5 @@ set -euxo pipefail
 SCRIPT_DIR="$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")"
 cd "$SCRIPT_DIR"
 
-docker build -f Dockerfile.image -t sourceacademy/ev3-source .
+docker build -t sourceacademy/ev3-source - < Dockerfile.image
 
--- build_sling.sh
+++ build_sling.sh
@@ -4,5 +4,6 @@ set -euxo pipefail
 
 SCRIPT_DIR="$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")"
 cd "$SCRIPT_DIR"
-docker build -f Dockerfile.sling -t sourceacademy/ev3-source-build .
-docker run --rm -it -v "$(realpath "$SCRIPT_DIR")":/src -w /src -u 0:0 --entrypoint /src/.docker_entrypoint.sh sourceacademy/ev3-source-build
+
+docker build -t sourceacademy/ev3-source-build - < Dockerfile.sling
+docker run --rm -v "$(realpath "$SCRIPT_DIR")":/src -w /src -u 0:0 --entrypoint /src/.docker_entrypoint.sh sourceacademy/ev3-source-build
