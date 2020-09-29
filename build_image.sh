0687f20ab0bee7002d93e99b65daf45ee61fefaf
angelsl <angelsl@in04.sg>
2020-09-30 03:54:21 +0800

    Build on tag and workflow dispatch only

--- build_image.sh
+++ build_image.sh
@@ -13,6 +13,7 @@ docker build -t sourceacademy/ev3-source .
 TMPDIR=$(mktemp -d)
 TARFILE="$TMPDIR/ev3-source.tar"
 IMGFILE="$TMPDIR/ev3-source.img"
+OUTFILE="$SCRIPT_DIR/ev3-source.img.zip"
 
 bash "$SCRIPT_DIR/brickstrap.sh" create-tar sourceacademy/ev3-source "$TARFILE"
 
@@ -24,6 +25,6 @@ echo 'Using BRICKSTRAP_IMAGE_FILE_SIZE='$BRICKSTRAP_IMAGE_FILE_SIZE
 bash "$SCRIPT_DIR/brickstrap.sh" create-image "$TARFILE" "$IMGFILE"
 rm "$TARFILE"
 
-mv "$IMGFILE" "$SCRIPT_DIR"
+zip -v "$OUTFILE" "$IMGFILE"
 rm -rf "$TMPDIR"
->&2 echo "Image created at $SCRIPT_DIR/$(basename "$IMGFILE")"
+>&2 echo "Image created at $OUTFILE"
