d30014c14a0cccd0a392b774600027e21278dcfd
angelsl <angelsl@in04.sg>
2020-10-01 05:08:01 +0800

    Fix docker_entrypoint

--- .docker_entrypoint.sh
+++ .docker_entrypoint.sh
@@ -5,7 +5,5 @@ set -euxo pipefail
 SCRIPT_DIR="$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")"
 cd "$SCRIPT_DIR"
 
-UIDGID=(-u \#$(stat --printf="%u" "$SCRIPT_DIR/build_sling.sh") -g \#$(stat --printf="%g" "$SCRIPT_DIR/build_sling.sh"))
-
-sudo $UIDGID "$SCRIPT_DIR/.docker_build.sh"
-sudo $UIDGID "$SCRIPT_DIR/.docker_build_panel.sh"
+sudo -u \#$(stat --printf="%u" "$SCRIPT_DIR/build_sling.sh") -g \#$(stat --printf="%g" "$SCRIPT_DIR/build_sling.sh") "$SCRIPT_DIR/.docker_build.sh"
+sudo -u \#$(stat --printf="%u" "$SCRIPT_DIR/build_sling.sh") -g \#$(stat --printf="%g" "$SCRIPT_DIR/build_sling.sh") "$SCRIPT_DIR/.docker_build_panel.sh"
