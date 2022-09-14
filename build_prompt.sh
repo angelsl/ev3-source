6d3d8e5ca5f72e8aaeaf6ab0e15490b179b87f75
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-09-15 03:23:35 +0800

    Update PEAP prompt submodule + build scripts
    * Add clean up commands to script
    * Improve script quality

--- build_prompt.sh
+++ build_prompt.sh
@@ -3,10 +3,15 @@
 set -euxo pipefail
 
 SCRIPT_DIR="$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")"
+REPO_DIR="ev3dev-peap-gui-login"
+IMAGE_NAME="sourceacademy/ev3-vala-compiler"
+
+cd "$SCRIPT_DIR/$REPO_DIR"
+docker build -t "$IMAGE_NAME" .
+
 cd "$SCRIPT_DIR"
-REPO_DIR="ev3-prompt-nusnet-credentials/vala"
-
 mkdir -p build-ev3/executables
+docker run --rm  -v "$(realpath "$SCRIPT_DIR")":/src -w /src -u 0:0 "$IMAGE_NAME" -o build-ev3/executables/nus_login "$REPO_DIR/main.vala"
 
-docker build -t sourceacademy/ev3-vala-compiler - < Dockerfile.prompt
-docker run --rm  -v "$(realpath "$SCRIPT_DIR")":/src -w /src -u 0:0 sourceacademy/ev3-vala-compiler -o build-ev3/executables/nus_login "$REPO_DIR/main.vala"
+# Clean up
+docker rmi "$IMAGE_NAME"
