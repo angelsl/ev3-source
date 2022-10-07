8e5039fc0cc00bdeb9a38de7355c659ce9268118
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2022-10-07 18:49:55 +0800

    Disable SSH on first boot

--- image/bootstrap.sh
+++ image/bootstrap.sh
@@ -37,9 +37,10 @@ dpkg -i busybox-static_1.22.0-19+b3_armel.deb
 systemctl disable uuidd.socket
 rm uuid-runtime_2.29.2-1+deb9u1_armel.deb busybox-static_1.22.0-19+b3_armel.deb
 
-# Install RTL8811CU drivers on first boot
+# Install RTL8811CU drivers, disable SSH on first boot
 sed -e '/^\s*;;$/{i \
         depmod\
+        systemctl disable ssh\
         reboot' \
     -e ':a;n;ba}' -i /etc/init.d/firstboot
 
